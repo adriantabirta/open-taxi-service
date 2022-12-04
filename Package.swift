bde4e8f237b04b519434edd3e2682112681d3773
at <adriantabirta@gmail.com>
2022-12-04 23:51:04 +0200

    static

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "TaxifyNetworking", type: .dynamic, targets: ["TaxifyNetworking"]),
+        .library(name: "TaxifyNetworking", type: .static, targets: ["TaxifyNetworking"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
